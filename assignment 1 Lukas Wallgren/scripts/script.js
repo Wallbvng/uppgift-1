f3d125ad304b2a358698e6f86d8962438feed8e2
Wallbvng <ziggewa@live.se>
2024-10-08 19:05:40 +0200

    uppgift 1

---
+++ assignment 1 Lukas Wallgren/scripts/script.js
@@ -0,0 +1,21 @@
+
+function log(message){
+console.log(message);
+
+};
+
+let foo; 
+foo = 0;
+
+let bar;
+bar = 0;
+console.log(foo / bar);
+
+function divideNumbers() {
+
+if (bar === 0) {
+    alert("You can't divide us");}
+
+    else {
+    console.log(foo / bar);} 
+}
